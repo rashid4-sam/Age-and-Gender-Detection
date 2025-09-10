c04e4bec981b99d7fa6013f1c87d819e4a7d05ae
Muhammed Rashid Samad <rashidsamad70@gmail.com>
2025-09-10 22:59:10 +0530

    Update agegenderdetection.py

--- app/agegenderdetection.py
+++ app/agegenderdetection.py
@@ -2,12 +2,22 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+import gdown
+import os
+
+
+def download_model():
+    url = "https://drive.google.com/file/d/1ULJOtjtm6eGZj81_NWRVaIJcaIkuABJH/view?usp=drive_link"  
+    output = "model_11.h5"
+    if not os.path.exists(output):
+        gdown.download(url, output, quiet=False)
+    return output
 
 # Load the trained model
 @st.cache_resource
 def load_model():
-    return tf.keras.models.load_model("model_11.h5", compile=False)
-
+    model_path = download_model()
+    return tf.keras.models.load_model(model_path, compile=False)
 
 model = load_model()
 
@@ -20,13 +30,13 @@ uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     # Display uploaded image
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_container_width =True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     # Preprocess (resize to model input shape)
     img = image.convert("L").resize((128, 128))   # "L" means grayscale
     img_array = np.array(img) / 255.0
-    img_array = np.expand_dims(img_array, axis=-1)   # add channel dimension (128,128,1)
-    img_array = np.expand_dims(img_array, axis=0)    # add batch dimension (1,128,128,1)
+    img_array = np.expand_dims(img_array, axis=-1)   # add channel dimension
+    img_array = np.expand_dims(img_array, axis=0)    # add batch dimension
 
     # Make predictions
     gender_pred, age_pred = model.predict(img_array)
